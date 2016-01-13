e2f461d54abe960d2125d433f818734780993c76
Nephe <hetier.guillaume@gmail.com>
2016-01-13 15:56:18 +0100

    Binary operator interpretation

--- interpreter.ml
+++ interpreter.ml
@@ -24,13 +24,38 @@ let rec interpret_unary env op e =
     | Su_neg -> let v = interpret_expr env e in
     (match v with 
         | Sc_bool b -> Sc_bool (not b)
-        | _ -> failwith "Invalid type : St_bool expected")
+        | _ -> failwith "Invalid type : St_bool expected"
+        )
 
-    and interpret_binary env op a b = failwith "TODO"
+    and interpret_binary env op a b =
+    let compute_op op va vb =
+        match va, vb with
+        | Sc_int ia, Sc_int ib -> (match op with
+            | Sb_add -> Sc_int(Int64.add ia ib)
+            | Sb_sub -> Sc_int(Int64.sub ia ib)
+            | Sb_mul -> Sc_int(Int64.mul ia ib)
+            | Sb_div -> if ib = Int64.zero
+                        then failwith "Invalid operation : division by zero";
+                        Sc_int(Int64.div ia ib)
+            | Sb_lt  -> Sc_bool(ia < ib)
+            | _ -> failwith "Should not happen"
+            )
+        | _, _ -> failwith "Invalid type : St_int*St_int expected"
+    in
+    match op with
+        | Sb_or -> (match interpret_expr env a with
+            | Sc_bool b1 when b1 -> Sc_bool true
+            | Sc_bool b1 -> (match interpret_expr env b with
+                | Sc_bool b2 -> Sc_bool b2
+                | _ -> failwith "Invalid type Sc_bool*Sc_bool expected"
+                )
+            | _ -> failwith "Invalid type Sc_bool*Sc_bool expected"
+            )
+        | _ -> compute_op op (interpret_expr env a) (interpret_expr env b)
 
 
     and interpret_expr env (expr, ext) = 
-    match expr with
+        match expr with
     | Se_const e -> e
     | Se_random (a,b) -> Sc_int(Int64.add a (Random.int64 (Int64.sub b a)))
     | Se_var var -> failwith "TODO"
