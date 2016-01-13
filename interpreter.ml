4e4f8a787cbb207542665b872cd86b3cc8d6b0b8
Nephe <hetier.guillaume@gmail.com>
2016-01-13 16:29:23 +0100

    Interpretation of expressions, blocks and some instructions (if, assert..)

--- interpreter.ml
+++ interpreter.ml
@@ -57,9 +57,9 @@ let rec interpret_unary env op e =
             | Sc_bool b1 when b1 -> Sc_bool true
             | Sc_bool b1 -> (match interpret_expr env b with
                 | Sc_bool b2 -> Sc_bool b2
-                | _ -> failwith "Invalid type Sc_bool*Sc_bool expected"
+                | _ -> failwith "Invalid type : St_bool*St_bool expected"
                 )
-            | _ -> failwith "Invalid type Sc_bool*Sc_bool expected"
+            | _ -> failwith "Invalid type : St_bool*St_bool expected"
             )
         | _ -> compute_op op (interpret_expr env a) (interpret_expr env b)
 
@@ -68,11 +68,56 @@ let rec interpret_unary env op e =
  *)
     and interpret_expr env (expr, ext) = 
         match expr with
-    | Se_const e -> e
-    | Se_random (a,b) -> Sc_int(Int64.add a (Random.int64 (Int64.sub b a)))
-    | Se_var var -> failwith "TODO"
-    | Se_unary (op, e) -> interpret_unary env op e
-    | Se_binary (op, a, b) -> interpret_binary env op a b
+        | Se_const e -> e
+        | Se_random (a,b) -> Sc_int(Int64.add a (Random.int64 (Int64.sub b a)))
+        | Se_var var -> (match (try Hashtbl.find env.var var
+                        with Not_found -> failwith "Undeclared variable") with
+                        | Some v -> v
+                        | None -> failwith "Non initialized variable"
+                        )
+        | Se_unary (op, e) -> interpret_unary env op e
+        | Se_binary (op, a, b) -> interpret_binary env op a b
+
+
+(* 
+ * Interpret conditions
+*)
+let rec interpret_condition env cond blk1 blk2 =
+    match interpret_expr env cond with
+    | Sc_bool b when b -> interpret_block env blk1
+    | Sc_bool _ -> interpret_block env blk2
+    | _ -> failwith "Invalid type : St_bool expected"
+
+(*
+ * Interpret loops
+ *)
+and interpret_loop env cond blk = failwith "Todo"
+
+
+(*
+ * Interpret assert
+ *)
+and interpret_assert env expr =
+    match interpret_expr env expr with
+    | Sc_bool b when b -> ()
+    | Sc_bool _ -> failwith "Assert failure"
+    | _ -> failwith "Invalid type : St_bool expected"
+
+(*
+ * Interpret instructions
+ *)
+and interpret_command env cmd =
+    match cmd with
+    | Sc_assign (var, expr) -> failwith "Todo"
+    | Sc_if (cond, blk1, blk2) -> interpret_condition env cond blk1 blk2
+    | Sc_while (cond, blk) -> interpret_loop env cond blk
+    | Sc_proc_call proc_call -> failwith "TODO"
+    | Sc_assert expr -> interpret_assert env expr
+
+and interpret_block env blk =
+    match blk with
+    | [] -> ()
+    | (cmd, loc)::q -> interpret_command env cmd; interpret_block env q
 
 (*
  * List and initialize variable declaration
