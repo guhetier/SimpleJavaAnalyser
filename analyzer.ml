1d5caccdb1edf5539d27a920f7536080e13166e3
Nephe <hetier.guillaume@gmail.com>
2016-01-18 09:51:15 +0100

    Added interpretation in analyser option and cleaned file.

--- analyzer.ml
+++ analyzer.ml
@@ -1,29 +1,30 @@
 open Simple_java_syntax
 
+let print_file file = 
+    try
+        while true do
+            print_endline (input_line file)
+        done
+    with End_of_file -> seek_in file 0
+
 let main () =
     (* Parsing arguments *)
-    let f_name = ref "" and displayCode = ref false in
+    let f_name = ref "" and displayCode = ref false and interpret = ref false in
     Arg.parse [
-        ("--display", Arg.Set(displayCode), "Display program source")
+        ("--display", Arg.Set(displayCode), "Display program source");
+        ("--interpret", Arg.Set(interpret), "Interpret program source")
     ]
     (fun s -> f_name := s) "Mini-Java analyzer";
 
-  (* Parsing of the source file *)
-  let simple_java_prog =
-      if String.compare !f_name "" = 0 then failwith "no program file given";
+    (* Parsing of the source file *)
+    if String.compare !f_name "" = 0 then failwith "no program file given";
 
     let f_desc = open_in !f_name in
 
     (* Print program code if asked *)
-    if !displayCode then
-        begin
-            try
-                while true do
-                    print_endline (input_line f_desc)
-                done
-            with End_of_file -> seek_in f_desc 0;
-        end;
+    if !displayCode then print_file f_desc;
 
+    (* Parse and build an AST *)
     Localizing.current_file_name := !f_name;
     let lexbuf = Lexing.from_channel f_desc in
     let java_prog =
@@ -33,7 +34,11 @@ let main () =
               Printf.printf "Exception during parsing: %s\n" (Printexc.to_string e);
           failwith "Stopped"
     in
-    Simple_java_translate.tr_java_prog java_prog in
+    let s_prog = Simple_java_translate.tr_java_prog java_prog in
+
+    (* interpret program if asked *)
+    if !interpret then Interpreter.interpret_program s_prog;
+
   Printf.printf "finished...\n"
 
   let _ = main ()
